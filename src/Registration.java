2d627f5c4efc06028151b4f4662ea128616c4ff3
debbbl <22050034@siswa.um.edu.my>
2023-05-03 00:33:00 +0800

    update

--- src/Registration.java
+++ src/Registration.java
@@ -1,2 +1,58 @@
+import java.util.*;
+
 public class Registration {
+    private UserDatabase userDatabase;
+
+    public Registration(UserDatabase userDatabase) {
+        this.userDatabase = userDatabase;
+    }
+    public void showRegistrationForm() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Please enter your gender");
+        String gender=scanner.nextLine();
+        System.out.println("Please enter your email");
+        String email=scanner.nextLine();
+        System.out.println("Please enter your phone ");
+        String phone=scanner.nextLine();
+        System.out.println("Please enter your password");
+        String password=scanner.nextLine();
+        System.out.println("Please enter your birthday ");
+        String birthday=scanner.nextLine();
+        System.out.println("Please enter your address");
+        String address=scanner.nextLine();
+        System.out.println("Please enter your job");
+        String job=scanner.nextLine();
+        System.out.print("Enter hobbies (comma-separated): ");
+        String hobbiesString = scanner.nextLine();
+        List<String> hobbies = new ArrayList<>();
+        for (String hobby : hobbiesString.split(",")) {
+            hobbies.add(hobby.trim());
+        }
+        User.Builder builder = new User.Builder();
+        builder.email(email)
+                .phone(phone)
+                .username("username")
+                .password(password)
+                .hobbies(hobbies);
+        User user = builder.build();
+    }
+
+    public void submitRegistration(String username, String email, String phone, String password,
+                                   String name, String birthday, String gender, String address,
+                                   String job, List<String> hobbies) {
+        // Check if useraccount already exists
+        if (userDatabase.containsUser(username)) {//method in database
+            System.out.println("User exists");
+
+        }else {
+            userDatabase.add(user);//method in database
+            System.out.println("Registration successful!");
+        }
+        return;
+    }
+
+
+}
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,78 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+//register
 public class User {
+    private final String email;
+    private final String phone;
+    private final String username;
+    private final String password;
+    private final List<String> hobbies;
+
+    private User(Builder builder) {
+        this.email = builder.email;
+        this.phone = builder.phone;
+        this.username = builder.username;
+        this.password = builder.password;
+        this.hobbies = builder.hobbies;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public static class Builder {
+        private String email;
+        private String phone;
+        private String username;
+        private String password;
+        private List<String> hobbies;
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder hobbies(List<String> hobbies) {
+            this.hobbies = hobbies;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
+
