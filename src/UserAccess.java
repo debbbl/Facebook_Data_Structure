6835b54e5e9efecd5e19d6775385e22902113266
yerong liu <112962411+rexdliu@users.noreply.github.com>
2023-05-09 21:59:08 +0800

    Add files via upload

---
+++ src/UserAccess.java
@@ -0,0 +1,66 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class UserAccess extends User {
+    boolean isAdmin;
+    boolean isParent;
+    UserAccess parent;
+    private int maxTimeOfOneDay;//hour
+    private int remainingTime;
+
+
+    public void setMaxTimeOfOneDay(int maxTimeOfOneDay) {
+        this.maxTimeOfOneDay = maxTimeOfOneDay;
+    }
+
+    private List<User> userList;
+    private List<User> child;
+
+    public UserAccess(Builder builder, boolean isAdmin, boolean isParent, int maxTimeOfOneDay) {
+        super(builder);
+        this.isParent = isParent;
+        this.isAdmin = isAdmin;
+        this.maxTimeOfOneDay = maxTimeOfOneDay;
+        this.remainingTime = maxTimeOfOneDay;
+
+    }
+
+    public void delete(User userToBeDeleted) {
+        userList.remove(userToBeDeleted);
+    }
+
+
+    class parentControl {
+        public boolean CanChildContinueUse() {
+
+            if (isParent && isAdmin) {
+                int currentTime = 0;
+                parent.setMaxTimeOfOneDay(maxTimeOfOneDay);
+                for (int i = 0; i <= 24; i++) {
+                    currentTime++;
+                    remainingTime = (maxTimeOfOneDay - currentTime);
+                    if (remainingTime >=0) {
+                        System.out.println("Children can still use for "+ remainingTime +"hours");
+                    }
+                    if(remainingTime<0){
+                        System.out.printf("Stop ! The time of using is exceeded ");
+                        return false;
+
+                    }
+
+
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
+
+
+
+
+
