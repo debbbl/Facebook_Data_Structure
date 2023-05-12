27e3cfd6b9c7d67a9c8ccf8547ac949811d47395
Luqmanakm11 <u2100482@siswa.um.edu.my>
2023-05-12 16:07:08 +0800

    Update User.java

--- src/userAddFriend.java
+++ src/userAddFriend.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-
 public class userAddFriend {
     private String username;
     private ArrayList<String> friendList;
@@ -17,20 +16,14 @@ public class userAddFriend {
         this.recommendedFriends = new HashMap<>();
         this.friendRequestsQueue = new LinkedList<>();
     }
-
-    // Method for adding a new recommended friend to the graph
-    public void addRecommendedFriend(String friendUsername, int recommendationScore) {
-        // Check if the recommended friend is not already in the friend list or recommended list
-        if (!friendList.contains(friendUsername) && !recommendedFriends.containsKey(friendUsername)) {
+    public void addRecommendedFriend(String friendUsername, int recommendationScore) { // Method for adding a new recommended friend to the graph
+        if (!friendList.contains(friendUsername) && !recommendedFriends.containsKey(friendUsername)) { // Check if the recommended friend is not already in the friend list or recommended list
             recommendedFriends.put(friendUsername, recommendationScore);
             friendsGraph.put(friendUsername, new ArrayList<>());
         }
     }
-
-    // Method for accepting a friend request and adding the friend to the friend list
-    public void acceptFriendRequest(String friendUsername) {
-        // Check if the friend request exists in the friend request queue
-        if (friendRequestsQueue.contains(friendUsername)) {
+    public void acceptFriendRequest(String friendUsername) { // Method for accepting a friend request and adding the friend to the friend list
+        if (friendRequestsQueue.contains(friendUsername)) { // Check if the friend request exists in the friend request queue
             friendRequestsQueue.remove(friendUsername);
             friendList.add(friendUsername);
             friendsGraph.get(username).add(friendUsername);
@@ -38,18 +31,13 @@ public class userAddFriend {
             System.out.println(friendUsername + " is now your friend!");
         }
     }
-
-    // Method for rejecting a friend request
-    public void rejectFriendRequest(String friendUsername) {
-        // Check if the friend request exists in the friend request queue
-        if (friendRequestsQueue.contains(friendUsername)) {
+    public void rejectFriendRequest(String friendUsername) { // Method for rejecting a friend request
+        if (friendRequestsQueue.contains(friendUsername)) { // Check if the friend request exists in the friend request queue
             friendRequestsQueue.remove(friendUsername);
             System.out.println("You have rejected the friend request from " + friendUsername);
         }
     }
-
-    // Method for getting the list of recommended friends sorted by recommendation score
-    public ArrayList<String> getRecommendedFriends() {
+    public ArrayList<String> getRecommendedFriends() { // Method for getting the list of recommended friends sorted by recommendation score
         ArrayList<String> sortedFriends = new ArrayList<>();
         recommendedFriends.entrySet()
                 .stream()
@@ -57,17 +45,12 @@ public class userAddFriend {
                 .forEach(entry -> sortedFriends.add(entry.getKey()));
         return sortedFriends;
     }
-
-    // Method for getting the list of friend requests
-    public ArrayList<String> getFriendRequests() {
+    public ArrayList<String> getFriendRequests() { // Method for getting the list of friend requests
         ArrayList<String> requests = new ArrayList<>(friendRequestsQueue);
         return requests;
     }
-
-    // Method for sending a friend request to another user
-    public void sendFriendRequest(String friendUsername) {
-        // Check if the friend is not already in the friend list or friend request queue
-        if (!friendList.contains(friendUsername) && !friendRequestsQueue.contains(friendUsername)) {
+    public void sendFriendRequest(String friendUsername) { // Method for sending a friend request to another user
+        if (!friendList.contains(friendUsername) && !friendRequestsQueue.contains(friendUsername)) { // Check if the friend is not already in the friend list or friend request queue
             friendRequestsQueue.add(friendUsername);
             System.out.println("Friend request sent to " + friendUsername);
         }
